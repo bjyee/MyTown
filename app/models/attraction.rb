74eef733f3279b73c2b00c8815fa35b9e8aec0b8
Brandon Yee <brandon.yee88@gmail.com>
2012-11-22 10:12:20 -0500

    finished assignment

--- app/models/attraction.rb
+++ app/models/attraction.rb
@@ -3,4 +3,29 @@ class Attraction < ActiveRecord::Base
 	belongs_to :town
 	
 	scope :all, order('name')
+	
+	before_save :find_attraction_coordinates
+	
+	def create_map_link(zoom=13,width=800,height=800)
+		markers = ""; i = 1
+		self.attractions.all.each do |attr|
+			markers += "&markers=color:red%7Ccolor:red%7Clabel:#{i}%7C#{attr.lat},#{attr.lon}"
+			i += 1
+		end
+		map = "http://maps.google.com/maps/api/staticmap?center=#{lat},#{lon}&zoom=#{zoom}&size=#{width}x#{height}&maptype=#{markers}&sensor=false"
+	end
+	
+	private
+  
+    def find_attraction_coordinates
+      str = self.street
+      town = self.town.name
+      st = self.town.state
+      coord = Geokit::Geocoders::GoogleGeocoder.geocode "#{str}, #{town}, #{st}"
+      if coord.success
+  	    self.lat, self.lon = coord.ll.split(',')
+  	  else 
+  	    errors.add_to_base("Error with geocoding")
+  	  end
+    end
 end
--- app/models/town.rb
+++ app/models/town.rb
@@ -8,4 +8,26 @@ class Town < ActiveRecord::Base
 	
 	accepts_nested_attributes_for :attractions, :reject_if => lambda { |attraction| attraction[:name].blank? }
 	
+	before_save :find_town_coordinates
+	
+	def create_map_link(zoom=12,width=800,height=800)
+		markers = ""; i = 1
+		self.attractions.all.each do |attr|
+			markers += "&markers=color:red%7Ccolor:red%7Clabel:#{i}%7C#{attr.lat},#{attr.lon}"
+			i += 1
+		end
+		map = "http://maps.google.com/maps/api/staticmap?center=#{lat},#{lon}&zoom=#{zoom}&size=#{width}x#{height}&maptype=#{markers}&sensor=false"
+	end
+	
+	private
+	
+	def find_town_coordinates
+		coord = Geokit::Geocoders::GoogleGeocoder.geocode "#{name}, #{state}"
+		if coord.success
+			self.lat, self.lon = coord.ll.split(',')
+		else
+			errors.add_to_base("Error with geocoding")
+		end
+	end
+	
 end
